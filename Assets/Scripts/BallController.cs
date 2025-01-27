669999df41a667e1474919abbb791106d518b175
Jeroen <s1137642@student.hsleiden.nl>
2025-01-27 21:06:04 +0100

    feat: check ball for out of bounds

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,20 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using MixedReality.Toolkit;
+using MixedReality.Toolkit.Subsystems;
+using UnityEngine.XR;
 
 public class BallController : MonoBehaviour
 {
+    [SerializeField] private GameObject ball;
+    [SerializeField] private GameObject recallPoint;
 
-    private void Ray(){
-        RaycastHit hit;
-        float distance = 20f;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, distance))
-        { 
-            Debug.Log("Throw Event");
+    private HandsAggregatorSubsystem aggregator;
+    private LayerMask groundLayerMask;
+    private int maxHeight = 10;
+    public bool BallInPlay = false;
+
+    public void OnBallReleased(){
+        BallInPlay = true;
+    }
+
+    public void RecallBall(){
+        ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        ball.transform.position = recallPoint.transform.position;
+        BallInPlay = false;
+    }
+
+    private void FixedUpdate(){
+        if (BallInPlay){
+            CheckForOutOfBounds();
         }
-        else
+    }
+
+    private void CheckForOutOfBounds(){
+        RaycastHit hit;
+        if (!Physics.Raycast(ball.transform.position, Vector3.down, out hit, maxHeight, groundLayerMask))
         { 
-            Debug.Log("Reset Ball"); 
+            RecallBall();
+        }
+    }
+
+    private void Start(){
+        aggregator = XRSubsystemHelpers.GetFirstRunningSubsystem<HandsAggregatorSubsystem>();
+        groundLayerMask = LayerMask.GetMask("Ground");
+    }
+
+    private void Update(){
+        Test();
+    }
+
+    private void Test(){
+        bool handIsValid = aggregator.TryGetPalmFacingAway(XRNode.LeftHand, out bool isLeftPalmFacingAway);
+        if (isLeftPalmFacingAway){
+            Debug.Log("Left palm facing away");
         }
     }
 }
